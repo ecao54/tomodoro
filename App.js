917e40ac800e8bc753ec6453897b855685d4159d
Skyla Jin <skylajin@gmail.com>
2025-01-04 00:41:56 -0800

    fixed timer

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from "./app/screens/HomeScreen";
-import ButtonBar from './app/components/ButtonBar';
 
 export default function App() {
   return (
