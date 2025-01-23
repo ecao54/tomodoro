5b45dbb91899ee3d2de4fa836eea3da67baa80d0
ecao54 <47088137+ecao54@users.noreply.github.com>
2025-01-23 01:21:23 -0600

    major statsScreen reformatting, graph functionailty, etc

--- server/models/Stats.js
+++ server/models/Stats.js
@@ -1,5 +1,22 @@
 const mongoose = require('mongoose');
 
+const studySessionSchema = new mongoose.Schema({
+    timestamp: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ['tomato', 'plant'],
+        required: true
+    }
+});
+
 const statsSchema = new mongoose.Schema({
     userId: {
         type: String,
@@ -25,7 +42,8 @@ const statsSchema = new mongoose.Schema({
     lastStudyDate: {
         type: Date,
         default: Date.now
-    }
+    },
+    sessions: [studySessionSchema]
 });
 
 module.exports = mongoose.model('Stats', statsSchema);
